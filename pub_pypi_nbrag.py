f2bb08716f74b9a34d017e55d40db3486c8755c0
LAPTOP-7V78BBO2\ydf19 <ydf@xx.com>
2026-06-07 19:11:43 +0800

    commit

--- pub_pypi_nbrag.py
+++ pub_pypi_nbrag.py
@@ -7,13 +7,15 @@ import git_nbrag
 
 # sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+python = sys.executable
+
 t_start = time.time()
-os.system("python -m build")
+os.system(f'"{python}" -m build')
 t_end = time.time()
 print(f"build time: {t_end - t_start}")
 
 t_start = time.time()
-os.system("twine upload dist/*")
+os.system(f'"{python}" -m twine upload dist/*')
 t_end = time.time()
 print(f"upload time: {t_end - t_start}")
 
